51aef4081adbd35ee01f02727104f5ab96222407
root <root@exabook.(none)>
2009-06-08 10:15:42 +0200

    [PATCH] do not create config when there's one present

--- install.sh
+++ install.sh
@@ -9,8 +9,13 @@ mkdir -p ${dest}${pfx}/lib/orthos
 $CP ./skins/*.so ${dest}${pfx}/lib/orthos/
 
 conffile="${dest}/etc/orthos.conf"
-echo > ${conffile}
-echo -e "sessions\tfailsafe:/usr/bin/xterm:clock:/usr/bin/xclock" >> ${conffile}
-echo -e "Xserver\t/usr/bin/Xorg" >> ${conffile}
-echo -e "skin\t$pfx/lib/orthos/libdefault.so" >> ${conffile}
+if [ -f "$conffile" ] ; then
+	echo "config file found, not creating it"
+else
+	echo "creating default config file"
+	echo > ${conffile}
+	echo -e "sessions\tfailsafe:/usr/bin/xterm:clock:/usr/bin/xclock" >> ${conffile}
+	echo -e "Xserver\t/usr/bin/Xorg" >> ${conffile}
+	echo -e "skin\t$pfx/lib/orthos/libdefault.so" >> ${conffile}
+fi
 
