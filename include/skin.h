7cd9031f486bb0b2d850036d047cb601a218a7b8
Mirek Kratochvil <exa.exa@gmail.com>
2010-06-05 14:40:56 +0200

    skin header

---
+++ include/skin.h
@@ -0,0 +1,38 @@
+#ifndef ORTHOS_SKIN_H
+#define ORTHOS_SKIN_H
+
+extern "C"
+{
+
+	typedef int (*orthos_validate_login_func) (const char*, const char*);
+	typedef int (*orthos_do_login_func) (const char*, const char*);
+	typedef int (*orthos_action_func) (const char*);
+	typedef const char* (*orthos_get_config_func) (const char*);
+
+	typedef int (*skin_init_func) (orthos_validate_login_func,
+	                               orthos_do_login_func,
+	                               orthos_action_func,
+	                               orthos_get_config_func);
+	typedef int (*skin_fini_func) ();
+	typedef int (*skin_run_func) ();
+
+
+	int orthos_skin_init (orthos_validate_login_func,
+	                      orthos_do_login_func,
+	                      orthos_action_func,
+	                      orthos_get_config_func);
+	int orthos_skin_fini();
+
+	int orthos_skin_run();
+
+
+	int orthos_validate_login (const char*name, const char*pw);
+	int orthos_choose_login (const char*name, const char* session);
+	int orthos_choose_action (const char* command);
+	const char* orthos_get_config (const char*);
+
+} //extern "C"
+
+#endif
+
+
